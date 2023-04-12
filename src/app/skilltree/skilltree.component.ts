ef215a2dcc039720051ea6967b7d66e911bfdd6b
Niklas Kahr <NiklasKahr>
2023-04-12 10:11:39 +0200

    skilltree

---
+++ src/app/skilltree/skilltree.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skilltree',
+  templateUrl: './skilltree.component.html',
+  styleUrls: ['./skilltree.component.scss']
+})
+export class SkilltreeComponent {
+
+}
