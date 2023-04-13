cbac6834c2e9c8a2b4c9f264e82aeaec81cb359f
Niklas Kahr <NiklasKahr>
2023-04-13 10:17:35 +0200

    skilltree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SkilltreeComponent } from './skilltree/skilltree.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SkilltreeComponent
+    SkilltreeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
